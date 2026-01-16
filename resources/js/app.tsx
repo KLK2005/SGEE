dde7207ac4657b41dec7fab924f642b8ebffa31f
koloko junior <juniorkoloko4@gmail.com>
2026-01-16 11:13:17 +0100

    fix: SÃ©paration app.tsx (point d'entrÃ©e) et App.tsx (routes)

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import AppComponent from './App';
+import App from './App';
 import './bootstrap';
 
 const root = ReactDOM.createRoot(document.getElementById('app') as HTMLElement);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <AppComponent />
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
